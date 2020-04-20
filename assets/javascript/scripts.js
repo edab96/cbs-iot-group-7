5ec4bdd266ad2bbafb9de4d970b5b92e090168b8
Edoardo Abate <47771746+edab96@users.noreply.github.com>
2020-04-20 12:22:36 +0200

    Adjusted API request, added parsing for the response payload, adjusted landing page content

--- assets/javascript/scripts.js
+++ assets/javascript/scripts.js
@@ -1,24 +1,33 @@
-var apiLogin = "5e99586c2564325c7bf94be0";
-var password = "c67f888a300313e72df4c79836869ef8";
-var myDeviceId = "1D88C8";
-var getMessagesURL1 = "https://api.sigfox.com/v2/devices/";
-var getMessagesURL2 = "/messages";
+/*
+Credentials otbained from Backend SigFox / Group / API Access
+The API Access should have reading permissions for the profile "message"
+It is important you specify your Device ID
+*/
 
-var authorizationBasic = window.btoa(apiLogin + ':' + password);
+var credentials = {
+    apiLogin: "5e9d7401e833d96cf4710c02",
+    password: "fab8b7cf61522b1a60ada26c06a32a22",
+    myDeviceId: "1D806D"
+}
 
+// Do not change the endpoint and path for the request
+var endpoint = "https://api.sigfox.com/v2/devices/";
+var path = "/messages";
+
+var authorizationBasic = window.btoa(credentials.apiLogin + ':' + credentials.password);
 var request = new XMLHttpRequest();
 var response;
 
 
-$( document ).ready(function() {
+$(document).ready(function () {
     $(".load-data").click(function () {
         getPlantData();
     });
-    
+
 });
 
-function getPlantData() { 
-    request.open('Get', getMessagesURL1 + myDeviceId + getMessagesURL2 , true, apiLogin, password);
+function getPlantData() {
+    request.open('Get', endpoint + credentials.myDeviceId + path, true, credentials.apiLogin, credentials.password);
     request.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded; charset=UTF-8');
     request.setRequestHeader('Authorization', 'Basic ' + authorizationBasic);
     request.setRequestHeader('Accept', 'application/json');
@@ -27,16 +36,23 @@ function getPlantData() {
 
 request.onreadystatechange = function () {
     if (request.readyState == 4 && request.status == 200) {
-       response = JSON.parse(request.responseText)
-       console.log(response)
-       var latestEvent = response.data[0];
-       $('#temperature-data').html("16" + "°C");
-       $('#moisture-data').html("45");
-       console.log(latestEvent);
-       messageDate = new Date(latestEvent.time);
-       $('#latest-update-data').html(messageDate);
+        response = JSON.parse(request.responseText)
+        console.log(response)
 
-       
+        var latestEvent = response.data[0];
+        console.log(latestEvent);
+
+        messageDate = new Date(latestEvent.time);
+
+        // Parse the hexadecimal data for temperature and moisture
+        var payload = latestEvent.data;
+        var temperature = parseInt(payload.slice(0, 2), 16);
+        var moisture = parseInt((payload.slice(4, 6) + payload.slice(2, 4)), 16);
+
+        // Update the app content with the parsed values and the date
+        $('#temperature-data').html(temperature + "°C");
+        $('#moisture-data').html(moisture);
+        $('#latest-update-data').html(messageDate);
 
     }
 };
