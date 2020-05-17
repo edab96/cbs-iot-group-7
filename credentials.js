347fd25f7abacba0274f5535a2218bfca3d24f12
Edoardo Abate <47771746+edab96@users.noreply.github.com>
2020-05-17 17:31:58 +0200

    Stripped API Credentials

--- credentials.js
+++ credentials.js
@@ -8,7 +8,7 @@ It is important you specify your Device ID
 */
 
 var credentials = {
-    apiLogin: "5e9d7401e833d96cf4710c02",
-    password: "23a48bfea0444a623d726ac1dce90dad",
-    myDeviceId: "1D806D"
+    apiLogin: "",
+    password: "",
+    myDeviceId: ""
 }
